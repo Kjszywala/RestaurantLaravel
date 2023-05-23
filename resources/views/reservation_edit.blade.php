510d5db1bda60e17f6dc8682459418f2cad99a5f
kjszywala <kjszywala@protonmail.com>
2023-05-23 18:26:24 +0100

    dziala i u mnie

--- resources/views/reservation_edit.blade.php
+++ resources/views/reservation_edit.blade.php
@@ -15,14 +15,14 @@
     @endif
         @csrf
     
-        <label for="text">Name:</label>
+        {{-- <label for="text">Name:</label>
         <input type="text" name="name" value="{{ $reservation->user->name }}"><br><br>
 
         <label for="text">Surname:</label>
         <input type="text" name="name" value="{{ $reservation->user->surname }}"><br><br>
 
         <label for="text">Phone:</label>
-        <input type="text" name="name" value="{{ $reservation->user->phone }}"><br><br>
+        <input type="text" name="name" value="{{ $reservation->user->phone }}"><br><br> --}}
 
         <label for="date">Date:</label>
         <input type="date" name="date" value="{{ $reservation->date }}"><br><br>
@@ -33,11 +33,11 @@
         <label for="party_size">Numer of guests:</label>
         <input type="number" name="party_size" value="{{ $reservation->party_size }}"><br><br>
 
-        <!-- <label for="user_id">ID user:</label>
+        <label for="user_id">ID user:</label>
         <input type="number" name="user_id" value="{{ $reservation->user_id }}"><br><br>
 
         <label for="table_id">ID table:</label>
-        <input type="text" name="table_id" value="{{ $reservation->table_id }}"><br><br> -->
+        <input type="text" name="table_id" value="{{ $reservation->table_id }}"><br><br> 
 
         <input type="submit" class="btn btn-success" value="Implement changes">
     </form>
