3d53e5f7721b792365c867433efb341ca27f64a0
kjszywala <kjszywala@protonmail.com>
2023-05-27 16:18:42 +0100

    Comments added to models

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -4,17 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * The Reservation class extends the Model class provided by Laravel's Eloquent ORM. 
+ * It represents a reservation in the system. 
+ * The $fillable property defines the attributes that can 
+ * be mass-assigned when creating or updating a reservation. The $table property 
+ * specifies the database table associated with the model.
+ */
 class Reservation extends Model
 {
     protected $fillable = ['id', 'date', 'time', 'party_size', 'user_id', 'table_id'];
     protected $table = 'reservations';
     public $timestamps = false;
 
+    /**
+     * Get the user associated with the reservation.
+     */
     public function user()
     {
         return $this->belongsTo(User::class, 'user_id');
     }
 
+    /**
+     * Get the table associated with the reservation.
+     */
     public function table()
     {
         return $this->belongsTo(Table::class, 'table_id');
--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * The User class extends the Model class provided by Laravel's Eloquent ORM. 
+ * It represents a user in the system. The $fillable property defines the attributes 
+ * that can be mass-assigned when creating or updating a user. 
+ * The $table property specifies the database table associated with the model.
+ */
 class User extends Model
 {
     //use HasFactory;
